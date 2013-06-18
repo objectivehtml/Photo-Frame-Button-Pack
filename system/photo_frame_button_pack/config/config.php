738b3263d5cc865ccb9a5e241b146bbcb4064c76
Justin Kimbrell <justin@objectivehtml.com>
2013-06-18 10:39:51 -0400

    Added version/config file

---
+++ system/photo_frame_button_pack/config/config.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['photo_frame_button_pack_version'] = '1.0';
+
+if(!defined('PHOTO_FRAME_BUTTON_PACK_VERSION'))
+{
+	define('PHOTO_FRAME_BUTTON_PACK_VERSION', $config['photo_frame_button_pack_version']);
+}
