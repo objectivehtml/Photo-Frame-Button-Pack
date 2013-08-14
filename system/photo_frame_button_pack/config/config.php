70f71776221bf112efdd8dccb361f2907a22bd98
Justin Kimbrell <justin@objectivehtml.com>
2013-08-14 11:26:02 -0400

    Updated version to 1.1.0

--- system/photo_frame_button_pack/config/config.php
+++ system/photo_frame_button_pack/config/config.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['photo_frame_button_pack_version'] = '1.0';
+$config['photo_frame_button_pack_version'] = '1.1.0';
 
 if(!defined('PHOTO_FRAME_BUTTON_PACK_VERSION'))
 {
